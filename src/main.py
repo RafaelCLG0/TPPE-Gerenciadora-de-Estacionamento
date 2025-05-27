42f27580cc62e602611779bfc308d0d7487c2230
RafaelCLG0 <rafaelworks32@gmail.com>
2025-05-27 15:33:18 -0300

    feat(api): adiciona endpoint GET /ping-db para teste de conexão com banco

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from src.api.v1.endpoints import acesso
 
 app = FastAPI()
 
-@app.get("/")
-def hello():
-    return {"message": "Hello World"}
+app.include_router(acesso.router)
+
+@app.get("/ping-db")
+async def ping_db():
+    return {"status": "OK"}
