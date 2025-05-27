aa706f832c4a8f0a1e766d49fc0cb21d33adcad9
RafaelCLG0 <rafaelworks32@gmail.com>
2025-05-27 15:34:38 -0300

    fix(init): cria tabela acessos no script ./db/init.sql do MySQL

--- db/init.sql
+++ db/init.sql
@@ -1,13 +1,7 @@
-CREATE TABLE IF NOT EXISTS veiculos (
+CREATE TABLE IF NOT EXISTS acessos (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    placa VARCHAR(10),
-    modelo VARCHAR(50)
+    placa VARCHAR(10) NOT NULL,
+    tipo_acesso VARCHAR(20) NOT NULL,
+    horario_entrada DATETIME NOT NULL,
+    horario_saida DATETIME
 );
-
-INSERT INTO veiculos (placa, modelo) VALUES ('DEF5678', 'Civic');
-INSERT INTO veiculos (placa, modelo) VALUES ('GHI9012', 'Corolla');
-INSERT INTO veiculos (placa, modelo) VALUES ('JKL3456', 'Fusca');
-INSERT INTO veiculos (placa, modelo) VALUES ('MNO7890', 'Palio');
-INSERT INTO veiculos (placa, modelo) VALUES ('PQR1234', 'Onix');
-INSERT INTO veiculos (placa, modelo) VALUES ('STU5678', 'HB20');
-INSERT INTO veiculos (placa, modelo) VALUES ('VWX9012', 'Sandero');
