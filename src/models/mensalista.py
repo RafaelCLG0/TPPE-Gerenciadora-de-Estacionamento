8436be3c900f90d8a0ce7d45bf4763717f78bed0
RafaelCLG0 <rafaelworks32@gmail.com>
2025-06-20 19:19:35 -0300

    refactor(pastas): Organizacao geral no sistema de pasta, deletando alguns arquivos e alterando outros.

--- src/models/mensalista.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer
-from src.models.base import Base
-
-class Mensalista(Base):
-    __tablename__ = "mensalistas"
-
-    id = Column(Integer, primary_key=True, index=True)
-    valor_mensal = Column(Integer)
--- test/unit/test_converter_tempo.py
+++
@@ -1,6 +0,0 @@
-from src.models.converter_tempo import ConverterTempo
-
-def test_converter_tempo():
-    ct = ConverterTempo()
-    assert ct.converter(10, 11) == 60
-    assert ct.converter(9, 12) == 180
