14e3ba4088d445523d900a746ea0aae9c6510fd3
RafaelCLG0 <rafaelworks32@gmail.com>
2025-07-11 22:10:23 -0300

    fix(script.js): adiciona URL do front-end ao CORS.

--- estacionamento-frontend/script.js
+++ estacionamento-frontend/script.js
@@ -1,7 +1,7 @@
 // =================================================================
 // CONFIGURATION
 // =================================================================
-const API_BASE_URL = 'http://localhost:8000';
+const API_BASE_URL = 'https://tppe-gerenciadora-de-estacionamento.onrender.com';
 
 // =================================================================
 // STATE MANAGEMENT
